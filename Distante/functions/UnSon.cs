0fac1c753bfcc112ea620549aae16209c299a85d
Beluchini <pavelg25052007@gmail.com>
2025-03-06 22:19:20 +0400

    Закладываю фундамент для 3 страницы

--- Distante/functions/UnSon.cs
+++ Distante/functions/UnSon.cs
@@ -2,12 +2,5 @@ namespace Distante.functions;
 
 public class UnSon
 {
-    public string Servise { get; set; }
-    public string Master { get; set; }
-
-    public UnSon(string servise, string master)
-    {
-        Servise = servise;
-        Master = master;
-    }
+    
 }
