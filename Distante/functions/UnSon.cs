a270933fa534cf70da138080903837a13fae9bd0
Beluchini <pavelg25052007@gmail.com>
2025-03-06 15:57:41 +0400

    Сделал первую страницу (дизайн 60%)

--- Distante/functions/UnSon.cs
+++ Distante/functions/UnSon.cs
@@ -3,4 +3,11 @@ namespace Distante.functions;
 public class UnSon
 {
     public string Servise { get; set; }
+    public string Master { get; set; }
+
+    public UnSon(string servise, string master)
+    {
+        Servise = servise;
+        Master = master;
+    }
 }
