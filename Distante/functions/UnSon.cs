5e03cf09b34343f02c45cdfeb4cd2f7e6c9b654e
Beluchini <pavelg25052007@gmail.com>
2025-03-08 17:27:41 +0400

    Написал логику 3 страницы

--- Distante/functions/UnSon.cs
+++ Distante/functions/UnSon.cs
@@ -2,5 +2,14 @@ namespace Distante.functions;
 
 public class UnSon
 {
-    
+    public string NewService { get; set; }
+    public string NewClient { get; set; }
+    public string NewDate { get; set; }
+
+    public UnSon(string newService, string newClient, string newDate)
+    {
+        NewService = newService;
+        NewClient = newClient;
+        NewDate = newDate;
+    }
 }
